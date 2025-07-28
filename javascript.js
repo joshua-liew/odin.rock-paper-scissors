dd503e7e7a09b2d9e63ad61934c0b22f4aa681fc
Joshua Liew <liqshann@gmail.com>
2025-07-28 17:08:51 +0900

    fix: remove choice function

--- javascript.js
+++ javascript.js
@@ -1,25 +1,3 @@
-function choice(n) {
-    // function that takes in number
-    // between 1 and 3
-    // and returns a string
-    let choice;
-    switch (n) {
-        case 0:
-            choice = "rock";
-            break;
-        case 1:
-            choice = "paper";
-            break;
-        case 2:
-            choice = "scissors";
-            break;
-        default:
-            throw new RangeError("Argument must be a number between 0 and 2");
-            break;
-    }
-    return choice;
-}
-
 function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);
     //console.log(rand);
