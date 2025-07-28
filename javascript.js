84feb0f4085c7f3f304c940ec25f76116911e1c6
Joshua Liew <liqshann@gmail.com>
2025-07-28 14:28:53 +0900

    feat: add getComputerChoice function that returns choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,21 @@
-console.log("Hello world!");
+//console.log("Hello world!");
+
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    //console.log(rand);
+    let choice;
+    switch (rand) {
+        case 0:
+            choice = "rock";
+            break;
+        case 1:
+            choice = "paper";
+            break;
+        case 2:
+            choice = "scissors";
+            break;
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
